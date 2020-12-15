045f4079ae6ddeebd23401751fe3b67c1315db98
vagnerrmello <vagnerrmello@gmail.com>
2020-12-15 01:25:11 -0300

    Adicionar arquivos de projeto.

---
+++ APICatalog_NetCore/Controllers/ProdutosController.cs
@@ -0,0 +1,81 @@
+﻿using APICatalog_NetCore.Context;
+using APICatalog_NetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalog_NetCore.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProdutosController(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            return _context.Produtos.AsNoTracking().ToList();
+        }
+
+        [HttpGet("{id}", Name = "ObterProduto")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.AsNoTracking().FirstOrDefault(p => p.CategoriaId == id);
+
+            if (produto == null)
+                return NotFound();
+
+            return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody]Produto produto)
+        {
+            //if (!ModelState.IsValid)
+            //{
+            //    return BadRequest(ModelState);
+            //}
+
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutId }, produto);
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult Put(int id, [FromBody] Produto produto)
+        {
+            if (id != produto.ProdutId)
+                return BadRequest();
+
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Produto> Delete(int id)
+        {
+            var produto = _context.Produtos.AsNoTracking().FirstOrDefault(p => p.CategoriaId == id);
+
+            //var produto = _context.Produtos.Find(id); /*A vantagem do find é que vai procurar primeiro na memória, se achar não vai procurar no banco, porém 
+                                                        /* o find só posso utilizar se o id for a chave primária da tabela*/
+
+            if (id != produto.ProdutId)
+                return BadRequest();
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ APICatalog_NetCore/Migrations/20201215023654_PopulaDb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APICatalog_NetCore.Migrations
+{
+    public partial class PopulaDb : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert Into Categorias(Nome, ImagemUrl) Values('Programação', 'https://raw.githubusercontent.com/json-api-dotnet/JsonApiDotnetCore/master/logo.png')");
+
+            mb.Sql("Insert Into Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, Categoria) " +
+                "Values('CSharo', 'Linguagem de programação', 299.99, 'https://raw.githubusercontent.com/json-api-dotnet/JsonApiDotnetCore/master/logo.png', 10, now(), " +
+                "(Select CategoriaId From Categorias Where Nome = 'Programação')");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete From Produtos");
+            mb.Sql("Delete From Categorias");
+        }
+    }
+}
